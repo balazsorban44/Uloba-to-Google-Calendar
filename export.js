772e7d9e74810fef3cd0f7f51dd0f973138417c0
Balázs Orbán <info@balazsorban.com>
2016-09-23 13:50:00 +0200

    Update export.js
    The script was exporting events with one date ahead.

--- export.js
+++ export.js
@@ -4,130 +4,129 @@
 // @version      0.1
 // @description  Uloba CSV export
 // @author       Balázs Orbán (info@balazsorban.com)
-// @match        https://arbeidsplan.uloba.no/Home/PersonIndexPrint
+// @match        https://arbeidsplan.uloba.no/Home/PersonIndexPrint*
 // @grant        none
 // ==/UserScript==
 
-// --------------------------------------MAIN FUNCTION-------------------------------------- //
-function main(){
-// ASKS USER FOR THE LOCATION OF THE EVENTS (OPTIONAL, SHOULD NOT CONTAIN "," BECAUSE OF THE PARSING FORMAT (CSV SEPARATES BY COMMAS))
-  var workLocation = window.prompt("Location(optional)(DO NOT USE COMMA!)");
 
-//FINDING THE EMPLOYER (ARBEIDSLEDER)
-  var employer;
-  var e = document.querySelectorAll('th, td');
-  for (var i = 0; i < e.length; i++) {
-    if (e[i].className == "assistantNameHeader" && e[i].innerText.length > 0 ){
-      employer = e[i+8].innerText;
-    }
-  }
-//console.log(employer); // WHO IS THE EMPLOYER?
 
-// GATHERING THE IMPORTANT DATA
-  var data = document.querySelectorAll('th,td');
-  var dataText = [];
-  for (i=0;i<data.length;i++){
-    dataText.push(data[i].innerText);
-  }
-
-// REFORMATTING DATES AND TIMES
-  for (i = 0; i < dataText.length; i++) {
-    var days =["Man", "Tir", "Ons", "Tor", "Fre", "Lør", "Søn"];
-    for (var j = 0; j < days.length; j++) {
-// HANDLING THE WORKING DATES
-      if (dataText[i].startsWith(days[j]) === true) {
-        dataText.splice(i,1,dataText[i].substr(4)); // SLICING DOWN THE UNIMPORTANT WORDS (DAY SHORTENINGS)
-        var dager = dataText[i].substr(0,2); // GETTING THE DAY OF THE MONTH
-        var mnd = dataText[i].substr(3,2); // GETTING THE MONTH OF THE YEAR
-        var aar = dataText[i].substr(6,2); // GETTING THE YEAR
-        dataText[i] = mnd + "/" + dager + "/" + "20" + aar; //NEW DATEFORMAT (FROM dd.MM.yy TO MM/dd/yyyy)
-      }
-    }
-
-// HANDLING THE WORKING HOURS
-    if (dataText[i].substr(6,1) == "-") {
-      dataText[i] = dataText[i].substr(0, dataText[i].length - 1); // SLICE DOWN THE LAST CHARACTER ( ⤶ )
-      dataText[i] = dataText[i].replace(/\./g,":"); //NEW TIMEFORMAT (FROM HH.mm TO HH:mm )
-    }
-  }
-
-//console.log(dataText); // CHECK THE THE DATA GATHERED SO FAR
-
-//SEPARATING WORKING DATES
-  var date = [];
-  function selectDate(toArray){
-    for (var i = 0; i < dataText.length - 7; i++) {
-      if (dataText[i+7].length == 13) {
-        toArray[i] = dataText[i];
-      }
-      else {
-        toArray[i] = "";
-      }
-    }
-  }
-  selectDate(date);
-
-//SEPARATING WORKING HOURS
-  var time = [];
-  function selectTime(toArray){
-    for (var i = 0; i < dataText.length; i++) {
-      if (dataText[i].length == 13) {
-        for (var j = 0; j < dataText.length; j++) {
-          toArray[i-7] = dataText[i];
+function exportGcal(){
+    var workLocation = window.prompt("Location(optional)(DO NOT USE COMMA!)");
+    var employer;
+    var e = document.querySelectorAll('th, td');
+    for (var i = 0; i < e.length; i++) {
+        if (e[i].className == "assistantNameHeader" && e[i].innerText.length > 0 ){
+              employer = e[i+8].innerText;
+            e[i].remove();
         }
-      }
-      else {
-        toArray[i] = "";
-      }
     }
-  }
-  selectTime(time);
-    // SEPARATING START FROM END TIME
-  var startTime = time.map((s) => s.substr(0, 5));
-  var endTime = time.map((s) => s.substr(8));
-
-// GATHERING ALL THE REFORMATTED DATA INTO THE CSV MULTIDIMENSIONAL-ARRAY
-  var csv = [[ "Subject","Start Date","Start Time","End Date","End Time","Location"],]; // HEADER FOR THE CSV FILE
-  for (i = 0; i < dataText.length; i++) {
-    // EVENT EXAMPLE:
-      // Event title: Work (Balázs Orbán)
-      // Start Date: 2016.07.12
-      // Start Time: 08:00
-      // End Date: 2016.07.12
-      // End Time: 16:00
-      // Location: Trondheim 7030
-    csv.splice(i+1,0,["Work (" + employer + ")",date[i], startTime[i],date[i],endTime[i],workLocation]);
-  }
-
-// REMOVING EMPTY LINES
-  var exportIt = [];
-  for (i = 0; i < csv.length; i++) {
-    if (csv[i][1] !== "" && typeof csv[i][1] !== 'undefined')  {
-      exportIt.push(csv[i]);
+    var data = document.querySelectorAll('th,td');
+    var dataText = [];
+    for (i=0;i<data.length;i++){
+        dataText.push(data[i].innerText);
     }
-  }
-//console.log(exportIt.join("\n")); // LAST CHECK BEFORE EXPORTING
+    var days =["Man", "Tir", "Ons", "Tor", "Fre", "Lør", "Søn"];
+    for (i = 0; i < dataText.length; i++) {
+        for (var j = 0; j < days.length; j++) {
+            if (dataText[i].startsWith(days[j]) === true) {
+                dataText.splice(i,1,dataText[i].substr(4));
+                var dager = dataText[i].substr(0,2);
+                var mnd = dataText[i].substr(3,2);
+                var aar = dataText[i].substr(6,2);
+                dataText[i] = mnd + "/" + dager + "/" + "20" + aar;
+            }
+        }
+        if (dataText[i].substr(6,1) == "-") {
+            dataText[i] = dataText[i].substr(0, dataText[i].length - 1);
+            dataText[i] = dataText[i].replace(/\./g,":");
+        }
+    }
+    //console.log(dataText);
 
-// PARSING INTO CSV FORMAT
-  var csvContent = "data:text/csv;charset=utf-8,";
-  exportIt.forEach(function(infoArray, index){
-    dataString = infoArray.join(",");
-    csvContent += index < exportIt.length ? dataString+ "\n" : dataString;
-  });
+    function listToMatrix(list, elementsPerSubArray) {
+        var matrix = [], i, k;
 
-// DOWNLOAD AS uloba.csv ON BUTTON CLICK
-  var encodedUri = encodeURI(csvContent);
-  button.setAttribute("href", encodedUri);
-  button.setAttribute("download", "uloba.csv");
-} // --------------------------------------END OF MAIN FUNCTION-------------------------------------- //
+        for (i = 0, k = -1; i < list.length; i++) {
+            if (i % elementsPerSubArray === 0) {
+                k++;
+                matrix[k] = [];
+            }
 
-// --------------------------------------INSERTING A BUTTON TO THE PAGE TO RUN THE MAIN FUNCTION-------------------------------------- //
-document.getElementsByTagName('h2')[1].innerHTML += "<a id='gcal' class='btn btn-primary' href='#'>Export as .csv (e.g. Google Calendar)</a>";
-var button = document.getElementById("gcal");
-button.style.float = "right";
-if (button) {
-    button.addEventListener ("click", main , false);
+            matrix[k].push(list[i]);
+        }
+
+        return matrix;
+    }
+
+    //GET THE DATES WHEN WORKING
+    var date = [];
+    function selectDate(toArray){
+        for (var i = 1; i < dataText.length - 6; i++) {
+            if (dataText[i+6].length == 13) {
+                toArray[i] = dataText[i-1];
+            }
+            else {
+                toArray[i] = "";
+            }
+        }
+    }
+    selectDate(date);
+
+    //GET THE DATES WHEN WORKING
+    var time = [];
+    function selectTime(toArray){
+        for (var i = 0; i < dataText.length; i++) {
+            if (dataText[i].length == 13) {
+                for (var j = 0; j < dataText.length; j++) {
+                    toArray[i-7] = dataText[i];
+                }
+            }
+            else {
+                toArray[i] = "";
+            }
+        }
+    }
+    selectTime(time);
+    var startTime = time.map((s) => s.substr(0, 5));
+    var endTime = time.map((s) => s.substr(8));
+
+
+
+
+    var csv = [[ "Subject","Start Date","Start Time","End Date","End Time","Location"],];
+    for (i = 0; i < dataText.length; i++) {
+        csv.splice(i+1,0,["Work (" + employer + ")",date[i], startTime[i],date[i],endTime[i],workLocation]);
+    }
+    var exportIt = [];
+    for (i = 0; i < csv.length; i++) {
+        if (csv[i][1] !== "" && typeof csv[i][1] !== 'undefined')  {
+            exportIt.push(csv[i]);
+        }
+    }
+
+    console.log(exportIt.join("\n"));
+    //console.log(employer);
+
+    var csvContent = "data:text/csv;charset=utf-8,";
+    exportIt.forEach(function(infoArray, index){
+
+        dataString = infoArray.join(",");
+        csvContent += index < exportIt.length ? dataString+ "\n" : dataString;
+
+    });
+    var encodedUri = encodeURI(csvContent);
+    button.setAttribute("href", encodedUri);
+    button.setAttribute("download", "uloba.csv");
 }
 
-// THANKS FOR USING MY CODE
-console.log("Thank you for running my code. If you have any questions, mail me: info@balazsorban.com ");
+document.getElementsByTagName('body')[0].innerHTML += "<a id='gcal' href='#'>Export to Google Calendar</a>";
+var button = document.getElementById("gcal");
+button.style.position ="fixed";
+button.style.right = "50px";
+button.style.top = "50px";
+button.style.backgroundColor = "darkblue";
+button.style.padding = "10px";
+button.style.color = "white";
+if (button) {
+    button.addEventListener ("click", exportGcal , false);
+}
